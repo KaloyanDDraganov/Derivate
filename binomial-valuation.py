3a53962bf1c729ca708b6234fee54d196f63585a
Kaloyan Draganov <kaloyanddraganov@gmail.com>
2024-06-26 16:43:19 +0200

    Added a cache.

--- binomial-valuation.py
+++ binomial-valuation.py
@@ -1,6 +1,9 @@
 import math
+import numpy as np
 
-def valuateOption(n: int, maturity: float, underlying: float, strike: float, sigma: float, rf: float) -> float:
+cache = dict()
+
+def valuateOption(n: int, maturity: np.complex128, underlying: np.complex128, strike: np.complex128, sigma: np.complex128, rf: np.complex128) -> np.complex128:
 
     if (n <= 0):
         return max(0, underlying - strike)
@@ -13,17 +16,28 @@ def valuateOption(n: int, maturity: float, underlying: float, strike: float, sig
     Pl = underlying * d
 
     q = (pow(1+rf, dT) - d) / (u - d)
-    Ch = valuateOption(n-1, maturity, Ph, strike, sigma, rf)
-    Cl = valuateOption(n-1, maturity, Pl, strike, sigma, rf)
 
+    if  Ph in cache:
+        Ch = cache.get(Ph)
+    else:
+        Ch = valuateOption(n-1, maturity, Ph, strike, sigma, rf)
+        cache.update({Ph:Ch})
 
-    c = (q*Ch + (1-q)*Cl) / (pow(1+rf, dT))
+    if  Pl in cache:
+        Cl = cache.get(Pl)
+    else:
+        Cl = valuateOption(n-1, maturity, Pl, strike, sigma, rf)
+        cache.update({Pl:Cl})
+
+    c = (q*Ch + (1- q) * Cl) / (pow(1+rf, dT))
     
     return c
 
 
 if __name__ == "__main__":
-   print(valuateOption(n=2,maturity=1,underlying=100,strike=90,sigma=10,rf=0.1))
+    print(valuateOption(n=30,maturity=1,underlying=100,strike=90,sigma=10,rf=0.1))
+    # EURIBOR 6 Monate = 3,671%
+    #print(valuateOption(n=100, maturity=0.5, underlying=18131.97, strike=19200, sigma=0.1216, rf=0.03671))
     
-def pointsToEur(points: int) -> float:
-    return 
+#def pointsToEur(points: int) -> np.complex128:
+    #return points / 1000
