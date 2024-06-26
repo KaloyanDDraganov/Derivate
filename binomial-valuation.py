cb3abb9b03a4c52b55a47d5ab7339481620aa556
Kaloyan Draganov <kaloyanddraganov@gmail.com>
2024-06-26 17:29:36 +0200

    Swap axes.

--- binomial-valuation.py
+++ binomial-valuation.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     n_values = list(range(1, 25))
     prices = list(map(lambda n: valuateOption(n, maturity=0.5, underlying=18131.97, strike=19200, sigma=0.1216, rf=0.03671), n_values))
 
-    plt.plot(prices, n_values, marker='o', linestyle='-', color='b', label='Option price against n')
+    plt.plot(n_values, prices, marker='o', linestyle='-', color='b', label='Option price against n')
     plt.xscale('log')
     plt.title("Calculating a call price with the binomial method")
     plt.xlabel("Log-Scaled n values")
